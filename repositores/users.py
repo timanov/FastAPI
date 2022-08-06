398b4b7f5c4b4e93a0f6dcbb7c380611845c3843
Timanov, Mikhail <mikhail.timanov@x5.ru>
2022-08-06 17:10:57 +0300

    Описание репозитория

---
+++ repositores/users.py
@@ -0,0 +1,19 @@
+from db.users import users
+from .base import BaseRepository
+
+class UserRepository(BaseRepository):
+
+    async def get_all(limit: int = 100, skip: int = 0):
+        return
+    
+    async def get_by_id(id: int):
+        return
+
+    async def create():
+        return
+    
+    async def update():
+        return
+
+    async def get_by_email(email: str):
+        return

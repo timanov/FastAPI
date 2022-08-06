869ebde79c10ee61de4159fd6d7d2334db32c4f3
Timanov, Mikhail <mikhail.timanov@x5.ru>
2022-08-06 17:22:11 +0300

    Интерфейс репозитория готов к реализации, готово model пользователя, что отправялет пользователь и что заполняем в БД

--- repositores/users.py
+++ repositores/users.py
@@ -1,19 +1,21 @@
+from typing import List
 from db.users import users
+from models.user import User, UserIn
 from .base import BaseRepository
 
 class UserRepository(BaseRepository):
 
-    async def get_all(limit: int = 100, skip: int = 0):
+    async def get_all(self, limit: int = 100, skip: int = 0) -> List[User]:
         return
     
-    async def get_by_id(id: int):
+    async def get_by_id(self, id: int) -> User:
         return
 
-    async def create():
+    async def create(self, u: UserIn) -> User:
         return
     
-    async def update():
+    async def update(self, u: UserIn) -> User:
         return
 
-    async def get_by_email(email: str):
+    async def get_by_email(self, email: str) -> User:
         return

a202bbfa442e8f92a2661ec682d7afeb873d13a4
Timanov, Mikhail <mikhail.timanov@x5.ru>
2022-08-06 18:53:25 +0300

    1/3 Реализация аутентификации с помощью JWT токена, криптографическое сравнивание

--- core/security.py
+++ core/security.py
@@ -1,4 +1,5 @@
 from passlib.context import CryptContext
+from jose import jwt
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +8,9 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hash: str) -> bool:
     return pwd_context.verify(password, hash)
+
+def create_access_token(data: dict) -> str:
+    return
+
+def decode_access_token(token: str):
+    return

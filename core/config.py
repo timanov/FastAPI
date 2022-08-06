e5928cd175e4b8a220cfdecbdd416fd7683e5411
Timanov, Mikhail <mikhail.timanov@x5.ru>
2022-08-06 22:21:38 +0300

    Добавление пути до БД

--- core/config.py
+++ core/config.py
@@ -2,7 +2,7 @@ from starlette.config import Config
 
 config = Config(".env")
 
-DATABASE_URL = config("EE_DATABASE_URL", cast=str, default="postgresql://postgres:postgres@localhost:5432/fast_api")
+DATABASE_URL = config("EE_DATABASE_URL", cast=str, default="postgresql://postgres:postgres@149.154.70.85:5432/fast_api")
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 ALGORITHM = "HS256"
 SECRET_KEY = config("EE_SECRET_KEY", cast=str, default="2b2d197649061838c0c381612cb117d5f562ff181f2ed68c7847471af22f83ce")

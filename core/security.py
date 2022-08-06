ab708541c0cb807cbb20a1a7890c0ebd380ed53e
Timanov, Mikhail <mikhail.timanov@x5.ru>
2022-08-06 20:15:39 +0300

    Реализация защиты

--- core/security.py
+++ core/security.py
@@ -1,4 +1,6 @@
 import datetime
+from fastapi import Request, HTTPException, status
+from fastapi.security import HTTPBearer
 from passlib.context import CryptContext
 from jose import jwt
 from .config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
@@ -18,7 +20,22 @@ def create_access_token(data: dict) -> str:
 
 def decode_access_token(token: str):
     try:
-        encoded_jwt = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        encoded_jwt = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     except jwt.JWSError:
         return None
-    return
+    return encoded_jwt
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        exp = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid auth token")
+        if credentials:
+            token = decode_access_token(credentials.credentials)
+            if token is None:
+                raise exp
+            return credentials.credentials
+        else:
+            raise exp 
